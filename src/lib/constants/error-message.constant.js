ea42b55ab75c836992cba534d54950e32d889f17
Yongjun Park <yopark@student.42seoul.kr>
2024-10-08 15:06:47 +0900

    feat(josa): install @toss/hangul, 메시지 중 등장하는 조사에 대한 처리

--- src/lib/constants/error-message.constant.js
+++ src/lib/constants/error-message.constant.js
@@ -1,7 +1,7 @@
 import { USER_INPUT } from '@/lib/constants/user-input.constant';
 
 const ERROR_MESSAGE = Object.freeze({
-  INVALID_YES_OR_NO: `잘못된 입력입니다. ${USER_INPUT.YES} 또는 ${USER_INPUT.NO}를 입력해주세요.`,
+  INVALID_YES_OR_NO: `잘못된 입력입니다. ${USER_INPUT.YES} 또는 ${USER_INPUT.NO}을 입력해주세요.`,
 });
 
 export { ERROR_MESSAGE };
--- src/views/output.view.constant.js
+++ src/views/output.view.constant.js
@@ -1,18 +1,20 @@
+import { josa } from '@toss/hangul';
+
 const CANDIDATE = Object.freeze({
-  PLAYER: '플레이어',
+  PLAYER: '플레이얼',
   DEALER: '딜러',
 });
 
 const OUTPUT_MESSAGE = Object.freeze({
   RECEIVE_CARD: ({ name, cardString }) =>
-    `${name}가 받은 카드는 ${cardString}입니다.`,
-  ACTION: ({ name, action }) => `${name}가 ${action}하였습니다.`,
-  BUST: (name) => `${name}가 버스트입니다.`,
+    `${josa(name, '이/가')} 받은 카드는 ${cardString}입니다.`,
+  ACTION: ({ name, action }) => `${josa(name, '이/가')} ${action}하였습니다.`,
+  BUST: (name) => `${josa(name, '이/가')} 버스트입니다.`,
   SUM_OF_CARDS: ({ name, sum }) => `${name} 카드의 합은 ${sum}입니다.`,
   START_GAME: '블랙잭 게임을 시작합니다.',
   PLAYER_WIN: 'WINNER WINNER CHICKEN DINNER 🍗',
   PLAYER_DRAW: '무승부입니다.',
-  PLAYER_LOSE: '플레이어가 패배하였습니다 😭',
+  PLAYER_LOSE: `${josa(CANDIDATE.PLAYER, '이/가')} 패배하였습니다 😭`,
   END_GAME: '블랙잭 게임을 종료합니다.',
   FINAL_RESULT: ({ totalGameCount, winCount, drawCount, loseCount }) =>
     `총 ${totalGameCount}판하였으며, ${winCount}승 ${drawCount}무 ${loseCount}패입니다.`,
